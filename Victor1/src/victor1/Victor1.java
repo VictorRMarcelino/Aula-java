82ba5e99d49abb77ddb69095ae283126638f745a
VICTOR RAMOS MARCELINO <victor.marcelino@alunos.sc.senac.br>
2021-08-02 22:12:32 -0300

    Cálculo de números inteiros

---
+++ Victor1/src/victor1/Victor1.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package victor1;
+
+/**
+ *
+ * @author victor.marcelino
+ */
+public class Victor1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String str1 = "Cálculos";
+        String str2 = "Cálculo de todos os números";
+        int a = 10;
+        int b = 20;
+        int c = 30;
+        int d = 40;
+        int e = a + b;
+        int f = a + c;
+        int g = a + d;
+        int h = b + c;
+        int i = b + d;
+        int j = c + d;
+        int k = a + b + c + d;
+        System.out.println(str1);
+        System.out.println("10+20 = " + e);
+        System.out.println("10+30 = " + f);
+        System.out.println("10+40 = " + g);
+        System.out.println("20+30 = " + h);
+        System.out.println("20+40 = " + i);
+        System.out.println("30+40 = " + j);
+        System.out.println(str2);
+        System.out.println("10+20+30+40 = " + k);
+        
+        
+    }
+    
+}
