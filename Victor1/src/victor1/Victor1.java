3f35d399094b8cbab24cc1525148c76a3e0b5268
VICTOR RAMOS MARCELINO <victor.marcelino@alunos.sc.senac.br>
2021-08-03 21:10:34 -0300

    Atualização da soma de números inteiros

--- Victor1/src/victor1/Victor1.java
+++ Victor1/src/victor1/Victor1.java
@@ -10,35 +10,102 @@ package victor1;
  * @author victor.marcelino
  */
 public class Victor1 {
+    int a = 10;
+    int b = 30;
+    int c = 50;
+    int d = 70;
+    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String str1 = "Cálculos";
-        String str2 = "Cálculo de todos os números";
-        int a = 10;
-        int b = 20;
-        int c = 30;
-        int d = 40;
-        int e = a + b;
-        int f = a + c;
-        int g = a + d;
-        int h = b + c;
-        int i = b + d;
-        int j = c + d;
-        int k = a + b + c + d;
+       Victor1 Val = new Victor1();
+        String str1 = "Número alvo: 40 ";
+        String str2 = "Realização primeiro cálculo:";
+        String str3 = "Realização segundo cálculo: ";
+        String str4 = "Realização terceiro cálculo: ";
+        String str5 = "Realização quarto cálculo: ";
+        String str6 = "Realização quinto cálculo:";
+        String str7 = "Realização sexto cálculo:";
+        String str8 = "Realização Sétimo cálculo";
+        String str9 = "Realização oitavo cálculo";
+        int[] ListNum = {10, 30, 50, 70};
+        int resp = 40;
+        
         System.out.println(str1);
-        System.out.println("10+20 = " + e);
-        System.out.println("10+30 = " + f);
-        System.out.println("10+40 = " + g);
-        System.out.println("20+30 = " + h);
-        System.out.println("20+40 = " + i);
-        System.out.println("30+40 = " + j);
         System.out.println(str2);
-        System.out.println("10+20+30+40 = " + k);
+        System.out.printf("10 + 30 = %d ", (Val.a + Val.b));
+         if(ListNum[0] + ListNum[1] == resp){
+             System.out.println("- Resposta correta\n" );
+         }
+         else{
+             System.out.println("- Resposta Incorreta");
+             
+         }
+         
+             
+        System.out.println(str1);
+        System.out.println(str3);
+        System.out.printf("10 + 50 = %d ", (Val.a + Val.c));
+         if(ListNum[0] + ListNum[2] == resp){
+             System.out.println("- Resposta correta " );
+         }
+         else{
+             System.out.println("- Resposta Incorreta\n");
         
+         }
+         
+
+        System.out.println(str1);
+        System.out.println(str4);
+        System.out.printf("10 + 70 = %d ", (Val.a + Val.d));
+         if(ListNum[0] + ListNum[3] == resp){
+             System.out.println("- Resposta correta " );
+         }
+         else{
+             System.out.println("- Resposta Incorreta\n");
         
+         }
+         
+         
+        System.out.println(str1);
+        System.out.println(str5);
+        System.out.printf("30 + 50 = %d ", (Val.b + Val.c));
+         if(ListNum[1] + ListNum[2] == resp){
+             System.out.println("- Resposta correta " );
+         }
+         else{
+             System.out.println("- Resposta Incorreta\n");
+        
+         }
+         
+         
+        System.out.println(str1);
+        System.out.println(str6);
+        System.out.printf("30 + 70 = %d ", (Val.b + Val.d));
+         if(ListNum[1] + ListNum[3] == resp){
+             System.out.println("- Resposta correta " );
+         }
+         else{
+             System.out.println("- Resposta Incorreta\n");
+        
+         }
+         
+        System.out.println(str1);
+        System.out.println(str7);
+        System.out.printf("50 + 70 = %d ", (Val.c + Val.d));
+         if(ListNum[2] + ListNum[3] == resp){
+             System.out.println("- Resposta correta " );
+         }
+         else{
+             System.out.println("- Resposta Incorreta\n");
+        
+         }
     }
-    
 }
+
+
+
+    
+
